25a1c0013eecbd527f6c3f8b39c5a2b3befa84b2
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-12-09 17:13:14 -0800

    added callback - push before migrate

--- data/migrations/20211210004821_recipes.js
+++ data/migrations/20211210004821_recipes.js
@@ -1,10 +1,18 @@
 
 exports.up = async function(knex) {
   await knex.schema
-    .createTable('recipes')
-    .createTable('steps')
-    .createTable('ingredients')
-    .createTable('steps_ingredients')
+    .createTable('recipes', table => {
+        table.increments('recipe_id')
+    })
+    .createTable('steps', table => {
+        table.increments('step_id')
+    })
+    .createTable('ingredients', table => {
+        table.increments('ingredient_id')
+    })
+    .createTable('steps_ingredients', table => {
+        table.increments('step_ingredient_id')
+    })
 };
 
 exports.down = async function(knex) {
